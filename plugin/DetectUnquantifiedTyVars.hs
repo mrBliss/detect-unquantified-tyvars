5c52236b463914f480f1090adc4761d9ed2af7f8
Thomas Winant <thomas.winant@cs.kuleuven.be>
2018-06-28 16:42:06 +0200

    Handle nested bindings, class methods, instance signatures, ...
    This requires more manual AST walking.

--- plugin/DetectUnquantifiedTyVars.hs
+++ plugin/DetectUnquantifiedTyVars.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 module DetectUnquantifiedTyVars (plugin) where
 
 -- base
@@ -11,11 +14,14 @@ import Data.Foldable (toList)
 import Data.Set (Set)
 import qualified Data.Set as Set
 
+-- mtl
+import Control.Monad.Reader (MonadReader, ReaderT, runReaderT, ask, local)
+import Control.Monad.Trans (lift)
+
 -- ghc
 import qualified GHC
 import qualified GhcPlugins as GHC
 import Maybes (orElse)
-import qualified TcEnv as GHC
 import qualified Bag as GHC
 import qualified TcRnMonad as GHC
 
@@ -23,7 +29,7 @@ import qualified TcRnMonad as GHC
 import Data.Generics (Data, everythingWithContext, mkQ)
 
 
--- * The plugin
+-- | The plugin
 plugin :: GHC.Plugin
 plugin = GHC.defaultPlugin
     { GHC.renamedResultAction = Just renamerPlugin
@@ -34,54 +40,177 @@ renamerPlugin :: [GHC.CommandLineOption]
               -> GHC.RenamedSource
               -> GHC.TcM ()
 renamerPlugin _cmdLineOpts _modSummary (hsGroup, _, _, _) = do
-    let GHC.HsGroup { GHC.hs_valds = hs_valds
-                    , GHC.hs_tyclds = hs_tyclds } = hsGroup
-    -- TODO hs_tyclds :: [TyClGroup GhcRn]
-    case hs_valds of
-        GHC.ValBinds {} -> return ()
-        GHC.XValBindsLR (GHC.NValBinds valBindGroups sigs) -> do
-            -- For each signature,
-            namesWithBndrs <- forM sigs $ \sig -> do
-                -- Collect all binders and the unquantified type variables
-                let (unquants, bndrs) = findUnquantifieds mempty sig
-                -- Warn about the unquantified type variables
-                mapM_ warnUnquantified $ unquantifiedTyVarsToList unquants
-                -- Return a mapping from each name that has this signature to
-                -- the binders of the signature
-                return (zip (getTypeSigNames sig) (repeat bndrs))
-            let nameToBndrs :: GHC.Name -> Binders
-                nameToBndrs name = lookup name (concat namesWithBndrs) `orElse` mempty
-                valBindToNames :: GHC.LHsBind GHC.GhcRn -> [GHC.Name]
-                valBindToNames = GHC.collectHsBindBinders . GHC.unLoc
-                valBinds :: [GHC.LHsBind GHC.GhcRn]
-                valBinds = concatMap (GHC.bagToList . snd) valBindGroups
-            -- For each value binding,
-            forM_ valBinds $ \valBind -> do
-                -- Get the binders corresponding with the name(s) of this
-                -- value binding
-                let valBindBndrs = foldMap nameToBndrs $ valBindToNames valBind
-                    (unquants, _) = findUnquantifieds valBindBndrs valBind
-                -- Warn about the unquantified type variables
-                mapM_ warnUnquantified $ unquantifiedTyVarsToList unquants
+    let GHC.HsGroup { GHC.hs_valds = valBinds
+                    , GHC.hs_tyclds = tyClGroups } = hsGroup
+    flip runReaderT mempty $ do
+        checkHsValBinds valBinds
+        mapM_ checkTyClGroup tyClGroups
 
+-- | Class for letting GHC print a warning.
+class Monad m => MonadWarning m where
+    warn :: GHC.SrcSpan -> GHC.SDoc -> m ()
 
-getTypeSigNames :: GHC.LSig GHC.GhcRn -> [GHC.Name]
-getTypeSigNames = GHC.nameSetElemsStable . GHC.getTypeSigNames . return
+instance MonadWarning GHC.TcM where
+    warn = GHC.addWarnAt GHC.NoReason
 
-warnUnquantified :: UnquantifiedTyVar -> GHC.TcM ()
+instance MonadWarning m => MonadWarning (ReaderT env m) where
+    warn loc msg = lift (warn loc msg)
+
+-- | Warn that the given type variable is unquantified.
+warnUnquantified :: MonadWarning m => UnquantifiedTyVar -> m ()
 warnUnquantified utv =
-    GHC.addWarnAt GHC.NoReason (unquantifiedTyVarSrcSpan utv) $
+    warn (unquantifiedTyVarSrcSpan utv) $
     GHC.text "Type variable without explicit forall:" GHC.<+>
     GHC.ppr utv
 
--- * Looking for unquantified tyvars in the AST
+-- | = Walking through the AST, checking for unquantified type variables
+--
+-- Unfortunately, we can't simply use the generic 'check' function, as the
+-- scoping rules of type variables are more complex:
+--
+-- * Type variables quantified in a type signature also scope over the body,
+--   which can contain pattern/expression signatures and type signatures for
+--   local bindings.
+--
+-- * Local bindings can nest arbitrarly deep, so we will need some recursion.
+--
+-- * A single type signature can be given for multiple bindings, e.g.:
+--
+--   @
+--       f, g :: forall a. a -> a
+--       f x = (x :: a)
+--       g (x :: a) = x
+--   @
+--
+-- * In class declarations, the type arguments to the class are also in scope.
+--
+-- * In instance declarations, any type variables in the instance head are
+--   also in scope.
+--
+-- For these reasons, we need to walk some parts of the AST manually instead
+-- of generically.
 
+-- | Generic function that warns about all unquantified type variables in
+-- every @'GHC.HsType' 'GHC.GhcRn'@ that occurs in @a@ using
+-- 'findUnquantifieds'.
+check :: (MonadReader Binders m, MonadWarning m, Data a) => a -> m ()
+check x = do
+    bndrs <- ask
+    let (unquants, _) = findUnquantifieds bndrs x
+    mapM_ warnUnquantified $ unquantifiedTyVarsToList unquants
+
+checkSigsAndHsBinds :: (MonadReader Binders m, MonadWarning m)
+                    => [GHC.LSig GHC.GhcRn] -> [GHC.HsBind GHC.GhcRn] -> m ()
+checkSigsAndHsBinds sigs binds = do
+    bndrs0 <- ask
+    -- For each signature,
+    namesWithBndrs <- forM sigs $ \sig -> do
+        -- Collect all binders and the unquantified type variables
+        let (unquants, bndrs) = findUnquantifieds bndrs0 sig
+        -- Warn about the unquantified type variables
+        mapM_ warnUnquantified $ unquantifiedTyVarsToList unquants
+        -- Return a mapping from each name that has this signature to
+        -- the binders of the signature
+        return (zip (getTypeSigNames sig) (repeat bndrs))
+
+    let nameToBndrs :: GHC.Name -> Binders
+        nameToBndrs name = lookup name (concat namesWithBndrs) `orElse` mempty
+
+    forM_ binds $ \bind ->
+        -- Get the binders corresponding with the name(s) of this value
+        -- binding
+        let bindBndrs = foldMap nameToBndrs $ bindToNames bind
+        in  local (bindBndrs <>) $ checkHsBind bind
+  where
+    bindToNames :: GHC.HsBind GHC.GhcRn -> [GHC.Name]
+    bindToNames = GHC.collectHsBindBinders
+
+-- | Variant of 'TcEnv.getTypeSigNames' that:
+-- * Takes one signature instead of a list.
+-- * Does not ignore a 'GHC.ClassOpSig'.
+-- * Returns a list of 'GHC.Name's instead of a 'GHC.NameSet'.
+getTypeSigNames :: GHC.LSig GHC.GhcRn -> [GHC.Name]
+getTypeSigNames lsig = map GHC.unLoc $ case GHC.unLoc lsig of
+    GHC.TypeSig _ names _      -> names
+    GHC.PatSynSig _ names _    -> names
+    GHC.ClassOpSig _ _ names _ -> names
+    _                          -> []
+
+checkHsValBinds :: (MonadReader Binders m, MonadWarning m)
+                => GHC.HsValBinds GHC.GhcRn -> m ()
+checkHsValBinds (GHC.ValBinds {}) = return ()  -- Only before renaming
+checkHsValBinds (GHC.XValBindsLR (GHC.NValBinds valBindGroups sigs)) =
+    checkSigsAndHsBinds sigs binds
+  where
+    binds :: [GHC.HsBind GHC.GhcRn]
+    binds = concatMap (map GHC.unLoc . GHC.bagToList . snd) valBindGroups
+
+checkHsBind :: (MonadReader Binders m, MonadWarning m)
+            => GHC.HsBind GHC.GhcRn -> m ()
+checkHsBind (GHC.FunBind { GHC.fun_matches = fun_matches }) = do
+    let GHC.MG { GHC.mg_alts = GHC.L _ lmatches } = fun_matches
+        matches = map GHC.unLoc lmatches
+    mapM_ check $ concatMap GHC.m_pats matches
+    mapM_ checkGRHSs $ map GHC.m_grhss matches
+checkHsBind (GHC.PatBind { GHC.pat_lhs = lpat, GHC.pat_rhs = grhss }) = do
+    check lpat
+    checkGRHSs grhss
+checkHsBind (GHC.VarBind { GHC.var_rhs = expr }) = check expr
+checkHsBind (GHC.PatSynBind _ (GHC.PSB { GHC.psb_def = lpat })) = check lpat
+checkHsBind _ = return ()
+
+checkGRHSs :: (MonadReader Binders m, MonadWarning m)
+           => GHC.GRHSs GHC.GhcRn (GHC.LHsExpr GHC.GhcRn) -> m ()
+checkGRHSs grhss = do
+    mapM_ check $ GHC.grhssGRHSs grhss
+    case GHC.unLoc $ GHC.grhssLocalBinds grhss of
+        GHC.HsValBinds _ valBinds -> checkHsValBinds valBinds
+        _ -> return ()
+
+checkTyClGroup :: (MonadReader Binders m, MonadWarning m)
+               => GHC.TyClGroup GHC.GhcRn -> m ()
+checkTyClGroup (GHC.XTyClGroup {}) = return ()
+checkTyClGroup (GHC.TyClGroup { GHC.group_tyclds = lTyClDecls
+                              , GHC.group_instds = lInstDecls }) = do
+    mapM_ (checkTyClDecl . GHC.unLoc) lTyClDecls
+    mapM_ (checkInstDecl . GHC.unLoc) lInstDecls
+
+checkTyClDecl :: (MonadReader Binders m, MonadWarning m)
+              => GHC.TyClDecl GHC.GhcRn -> m ()
+checkTyClDecl (GHC.ClassDecl { GHC.tcdTyVars = tyvars
+                             , GHC.tcdSigs = sigs
+                             , GHC.tcdMeths = lhsBinds }) = do
+    let binders = mkBinders $ GHC.hsAllLTyVarNames tyvars
+        binds = map GHC.unLoc $ GHC.bagToList lhsBinds
+    local (binders <>) $ checkSigsAndHsBinds sigs binds
+checkTyClDecl _ = return ()
+
+checkInstDecl :: (MonadReader Binders m, MonadWarning m)
+              => GHC.InstDecl GHC.GhcRn -> m ()
+checkInstDecl (GHC.ClsInstD _
+               (GHC.ClsInstDecl { GHC.cid_poly_ty = sigType
+                                , GHC.cid_binds = lhsBinds
+                                , GHC.cid_sigs = sigs })) = do
+    let tyvars = case sigType of
+            GHC.HsIB { GHC.hsib_ext = ibrn } -> GHC.hsib_vars ibrn
+            _   -> mempty
+        binders = mkBinders tyvars
+        binds = map GHC.unLoc $ GHC.bagToList lhsBinds
+    local (binders <>) $ checkSigsAndHsBinds sigs binds
+checkInstDecl _ = return ()
+
+
+-- | Generically find unquantified type variables in some piece of AST that
+-- contains @'GHC.HsType' 'GHC.GhcRn'@.
 findUnquantifieds :: Data a
-                  => Binders  -- ^ Type variables that are already bound
-                  -> a        -- ^ The thing (type, signature, ...) to walk over
+                  => Binders  -- ^ Type variables that are already bound/in
+                              -- scope.
+                  -> a        -- ^ The thing (type, signature, ...) to walk
+                              -- over
                   -> (UnquantifiedTyVars, Binders)
                      -- ^ The unquantified tyvars and all the binders
-                     -- encountered along the way, excluding the initial bounders
+                     -- encountered along the way, excluding the initial
+                     -- binders
 findUnquantifieds initBndrs =
     everythingWithContext initBndrs (<>) $
     mkQ (\stateBndrs -> (mempty, stateBndrs)) processType
@@ -104,17 +233,9 @@ findUnquantifieds initBndrs =
         = ((unitUnquantifiedTyVars $ mkUnquantifiedTyVar lname, mempty),
            stateBndrs)
     processType (GHC.HsForAllTy _ lbndrs _) stateBndrs
-        = let newBndrs = mkBinders $
-                         map (mkBinder . GHC.hsTyVarName . GHC.unLoc) lbndrs
+        = let newBndrs = mkBinders $ map (GHC.hsTyVarName . GHC.unLoc) lbndrs
           in  ((mempty, newBndrs), newBndrs <> stateBndrs)
     processType _ stateBndrs = (mempty, stateBndrs)
-
--- | The name of a type variable that occurs as a binder in a forall.
-newtype Binder = Binder (GHC.Name)
-    deriving (Eq, Ord)
-
-mkBinder :: GHC.Name -> Binder
-mkBinder = Binder
 
 -- | A collection of all the binders in scope.
 newtype Binders = Binders GHC.NameSet
@@ -123,7 +244,11 @@ newtype Binders = Binders GHC.NameSet
     -- it will not be equal (thanks to the renamer).
     deriving (Semigroup, Monoid)
 
-mkBinders :: [Binder] -> Binders
+-- | Useful for debugging
+instance GHC.Outputable Binders where
+    ppr (Binders names) = GHC.ppr (GHC.nameSetElemsStable names)
+
+mkBinders :: [GHC.Name] -> Binders
 mkBinders = coerce GHC.mkNameSet
 
 isQuantified :: GHC.Name -> Binders -> Bool
@@ -134,7 +259,7 @@ isUnquantified name = not . isQuantified name
 
 
 -- | The name of a type variable that is not explicitly bound or quantified by
--- a forall. This includes the precise of the type variable.
+-- a forall. This includes the location of the type variable.
 newtype UnquantifiedTyVar = UTV { unUTV :: GHC.Located GHC.Name }
 
 instance Eq UnquantifiedTyVar where
--- test/Test.hs
+++ test/Test.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS -fplugin=DetectUnquantifiedTyVars #-}
@@ -17,6 +18,15 @@ fun (ef :: e -> f) (es :: [x]) = fs
     fs = map ef es
     id2 :: a -> a
     id2 x = x
+    local :: forall t u. t -> u -> t
+    local (z :: t) _ = z :: t
+      where
+        id3 :: b -> b
+        id3 x = let id4 :: c -> c
+                    id4 y = y
+                    z' :: t
+                    z' = z
+                in x
 
 rank :: forall a. a -> a -> forall b. b -> b
 rank (x :: c) (y :: a) z = z
@@ -43,3 +53,20 @@ class Eq1 f where
 
 class Eq1 f => Ord1 f where
     compare1 :: forall a. Ord a => f a -> f a -> Ordering
+
+data Id a = Id a
+
+instance Eq1 Id where
+    eq1 :: Eq a => Id a -> Id a -> Bool
+    eq1 (Id x) (Id y) = x == y
+
+data Pha p a = Pha a
+
+instance Eq1 (Pha p) where
+    eq1 :: forall a. Eq a => Pha p a -> Pha p a -> Bool
+    eq1 (Pha x) (Pha y) = x == y
+      where
+        ida :: a -> a
+        ida x = x
+        idb :: b -> b
+        idb x = x
